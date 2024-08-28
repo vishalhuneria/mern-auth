21e8d424babe62d6773136bb8a7d53ea00cb28bf
vishalhuneria <126705907+vishalhuneria@users.noreply.github.com>
2024-08-29 00:14:05 +0530

    add and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import { Profile } from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
-    <div>App</div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path='/about' element={<Profile />}></Route>
+        <Route path='/sign-in' element={<Signin />}></Route>
+        <Route path='/sign-up' element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>Home hello
+
+        </div>
+    )
+}
+
+export default Home
